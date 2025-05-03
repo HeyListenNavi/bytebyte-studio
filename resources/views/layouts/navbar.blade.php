6b8f466fbf8c5f4ca3cb4458c47465fe7997b4c8
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2025-05-02 17:43:23 -0700

    Refactor navbar component for improved responsiveness and code clarity

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,27 +1,58 @@
 <nav
-    x-data="{ displayMenu: (window.innerWidth >= 807) ? true : false }"
-    x-resize="displayMenu = (window.innerWidth >= 807) ? true : false"
-    class="relative bg-glass flex w-full items-center justify-center p-8 shadow-2xl backdrop-blur-lg"
-    >
+    x-data="{ displayMenu: false }"
+    class="bg-glass relative flex w-full items-center justify-center p-8 shadow-2xl backdrop-blur-lg"
+>
     <div class="container flex items-center justify-between">
         <ul
             x-show="displayMenu"
-            x-bind:attribute="{ 'x-cloak': window.innerWidth <= 807 }"
-            x-on:click.outside="if (window.innerWidth <= 807) displayMenu = false"
+            x-cloak
+            x-on:click.outside="displayMenu = false"
             x-transition
-            class="absolute z-100 rounded-2xl lg:static top-[120%] left-1/2 -translate-x-1/2 lg:translate-0 w-11/12 lg:w-fit p-4 gap-6 bg-glass lg:bg-transparent backdrop-blur-lg lg:backdrop-blur-none divide-y lg:divide-x divide-secondary/20 lg:divide-y-0 lg:flex"
+            class="lg:hidden z-100 bg-glass divide-secondary/20 absolute left-1/2 top-[120%] w-11/12 -translate-x-1/2 gap-6 divide-y rounded-2xl p-4 backdrop-blur-lg"
         >
-            <div class="flex flex-col lg:flex-row gap-4 py-4 lg:py-0 lg:px-4 divide-y divide-secondary/20 lg:divide-y-0">
-                <h1 class="py-4 px-2 lg:hidden text-headline-large">ByteBite Studio</h1>
-                <li class="py-4 px-2 lg:py-0 lg:px-0"><x-button.text href="{{ route('home') }}" class="text-xl lg:text-body-small">Inicio</x-button.text></li>
-                <li class="py-4 px-2 lg:py-0 lg:px-0"><x-button.text href="{{ route('about-us') }}" class="text-xl lg:text-body-small">Nosotros</x-button.text></li>
-                <li class="py-4 px-2 lg:py-0 lg:px-0"><x-button.text href="{{ route('services') }}" class="text-xl lg:text-body-small">Servicios</x-button.text></li>
-                <li class="py-4 px-2 lg:py-0 lg:px-0"><x-button.text href="{{ route('faq') }}" class="text-xl lg:text-body-small">FAQ</x-button.text></li>
-                <li class="py-4 px-2 lg:py-0 lg:px-0"><x-button.text href="{{ route('policies') }}" class="text-xl lg:text-body-small">Politicas</x-button.text></li>
+            <div class="divide-secondary/20 flex flex-col gap-4 divide-y py-4">
+                <h1 class="text-headline-large px-2 py-4 lg:hidden">ByteBite Studio</h1>
+                <li class="px-2 py-4">
+                    <x-button.text href="{{ route('home') }}" class="text-xl">Inicio</x-button.text>
+                </li>
+                <li class="px-2 py-4">
+                    <x-button.text href="{{ route('about-us') }}" class="text-xl">Nosotros</x-button.text>
+                </li>
+                <li class="px-2 py-4">
+                    <x-button.text href="{{ route('services') }}" class="text-xl">Servicios</x-button.text>
+                </li>
+                <li class="px-2 py-4">
+                    <x-button.text href="{{ route('faq') }}" class="text-xl">FAQ</x-button.text>
+                </li>
+                <li class="px-2 py-4">
+                    <x-button.text href="{{ route('policies') }}" class="text-xl">Politicas</x-button.text>
+                </li>
             </div>
-            <li class="py-8 px-2 lg:py-0 lg:px-4"><x-button.text href="{{ route('portfolio') }}" class="text-xl lg:text-body-small">Portafolio</x-button.text></li>
+            <li class="px-2 py-8"><x-button.text href="{{ route('portfolio') }}"
+                    class="text-xl">Portafolio</x-button.text></li>
         </ul>
-        <div class="flex flex-row-reverse lg:flex-row items-center gap-4">
+        <ul class="hidden lg:flex p-4 w-fit divide-x divide-secondary/20">
+            <div class="divide-secondary/20 flex gap-4 flex-row divide-y-0 px-4 py-0">
+                <li class="px-0 py-0">
+                    <x-button.text href="{{ route('home') }}" class="text-body-small">Inicio</x-button.text>
+                </li>
+                <li class="px-0 py-0">
+                    <x-button.text href="{{ route('about-us') }}" class="text-body-small">Nosotros</x-button.text>
+                </li>
+                <li class="px-0 py-0">
+                    <x-button.text href="{{ route('services') }}" class="text-body-small">Servicios</x-button.text>
+                </li>
+                <li class="px-0 py-0">
+                    <x-button.text href="{{ route('faq') }}" class="text-body-small">FAQ</x-button.text>
+                </li>
+                <li class="px-0 py-0">
+                    <x-button.text href="{{ route('policies') }}" class="text-body-small">Politicas</x-button.text>
+                </li>
+            </div>
+            <li class="px-4 py-0"><x-button.text href="{{ route('portfolio') }}"
+                    class="text-body-small">Portafolio</x-button.text></li>
+        </ul>
+        <div class="flex flex-row-reverse items-center gap-4 lg:flex-row">
             <x-button.text>
                 <span>EN</span>
                 <x-bx-arrow-up-right></x-bx-arrow-up-right>
@@ -30,9 +61,7 @@
                 Contactanos
             </x-button>
         </div>
-        <x-button.text
-            x-on:click="displayMenu = !displayMenu"
-            class="lg:hidden text-headline-medium">
+        <x-button.text x-on:click="displayMenu = !displayMenu" class="text-headline-medium lg:hidden">
             <i class="bx bx-menu"></i>
         </x-button.text>
     </div>
